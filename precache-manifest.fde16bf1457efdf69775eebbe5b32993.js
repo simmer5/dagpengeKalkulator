b4bcd3013edcfd6aca1a707ee0aed3de549bc859
Mindaugas <strumke@yahoo.com>
2020-10-23 10:41:00 +0200

    Updates

---
+++ precache-manifest.fde16bf1457efdf69775eebbe5b32993.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6709b5ee12a7ac2735319ef77d53af9b",
+    "url": "/dagpengeKalkulator/index.html"
+  },
+  {
+    "revision": "9c098d84a46a01d1a392",
+    "url": "/dagpengeKalkulator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ec42b990bb71d9387ce4",
+    "url": "/dagpengeKalkulator/static/js/2.c5138438.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/dagpengeKalkulator/static/js/2.c5138438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c098d84a46a01d1a392",
+    "url": "/dagpengeKalkulator/static/js/main.d60698ba.chunk.js"
+  },
+  {
+    "revision": "5d8ef8cd976018db2350",
+    "url": "/dagpengeKalkulator/static/js/runtime-main.03ffff27.js"
+  }
+]);
